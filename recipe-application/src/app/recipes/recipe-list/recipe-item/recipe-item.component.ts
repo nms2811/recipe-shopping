357399661bca053d42a7b4c9dba8e13e92b69f12
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-01-21 23:36:01 -0500

    Added a sample data to check

--- recipe-application/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ recipe-application/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a Test',
+    'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVjaXBlfGVufDB8fDB8fA%3D%3D&w=1000&q=80' )
+  ];
   constructor() { }
 
   ngOnInit(): void {
