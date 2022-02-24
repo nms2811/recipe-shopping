3fae960a2b1879ae1d003dc93745dd5c5ba08f3c
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-02-24 17:27:49 -0500

    Added a shopping list

--- recipe-application/src/app/recipes/recipe.service.ts
+++ recipe-application/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredients.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -6,9 +7,17 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a Test',
-    'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVjaXBlfGVufDB8fDB8fA%3D%3D&w=1000&q=80' ),
+    'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVjaXBlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    [
+      new Ingredient('Meat', 1),
+      new Ingredient('French Fries', 20)
+    ]),
     new Recipe('Another Test Recipe', 'This is simply a Test',
-    'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVjaXBlfGVufDB8fDB8fA%3D%3D&w=1000&q=80' )
+    'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVjaXBlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    [
+      new Ingredient('Buns', 1),
+      new Ingredient('Meat', 20)
+    ])
   ];
 
   getRecipes() {
--- recipe-application/src/app/shopping-list/shopping-list.component.ts
+++ recipe-application/src/app/shopping-list/shopping-list.component.ts
@@ -1,24 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredients.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  providers: []
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 7)
-  ];
-
-  constructor() { }
+  ingredients: Ingredient[];
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit(): void {
+    this.ingredients = this.slService.getIngredients();
+    this.slService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    )
   }
 
-  onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
-
-  }
 }
